275a47f73db2fb069a653fccbf878f72c3c64288
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-01-13 11:00:28 -0500

    Refactor scroll behavior for smoother transitions on anchor links

--- js/script.js
+++ js/script.js
@@ -20,3 +20,15 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+// Scroll Mod to make smoother transition
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
